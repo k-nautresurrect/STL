3476bea28d8d741456b6034fa31caf7bbadacc1d
k-nautresurrect <kushignite@gmail.com>
2021-08-27 23:01:39 +0530

    Added nonmodifyingSeq files

---
+++ header_algorithm/nonModifyingSeq.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+int main(){
+
+	//initializing the array of only odd integer
+
+	int arr[]={3,5,7,9,11};
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+
+	auto all = all_of(arr, arr+n, [](int i){ return i%2; });
+	//takes 3 parameter starting ending and pred (UnaryPredicate).
+	//-> predicates are function that return boolean values.
+	//-> it takes a single parameter to decide the boolean value.
+
+	//the syntax is : 
+	// bool operator()
+
+	cout << all << endl;
+}
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
-all: findfunc binarySearch
+all: findfunc binarySearch nonModifyingSeq
 
 findfunc: header_algorithm/findFunc.cpp
 	g++ -std=c++14 -o findfunc header_algorithm/findFunc.cpp
 
 binarySearch: header_algorithm/binarySearch.cpp
 	g++ -std=c++14 -o binarySearch header_algorithm/binarySearch.cpp
+
+nonModifyingSeq: header_algorithm/nonModifyingSeq.cpp
+	g++ -std=c++14 -o nonModifyingSeq header_algorithm/nonModifyingSeq.cpp
